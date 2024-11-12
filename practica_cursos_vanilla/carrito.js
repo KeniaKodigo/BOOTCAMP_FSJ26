492c4715aa525f6a582c8aeb29698f16557fbc1c
KeniaKodigo <kenia.paz@kodigo.org>
2024-11-11 20:46:03 -0600

    funion agregar curso

--- practica_cursos_vanilla/carrito.js
+++ practica_cursos_vanilla/carrito.js
@@ -1,6 +1,40 @@
+import Swal from "sweetalert2";
+
 /**
  * agregar las clases al carrito
  * leer datos
  * eliminar una clase
  * vaciar el carrito
  */
+
+//metodo para agregar un curso
+//evento desencadenado (evento prederteminado)
+export function agregarCurso(e){
+    //solo tiene que devolver los elementos que contengan la clase "button-carrito"
+    if(e.target.classList.contains('button-carrito')){
+        //si esto es verdadero devuelve el boton seleccionado
+        console.log(e.target) //elemento html seleccionado
+
+        //parentElement
+        //devolvemos el elemento padre (abuelo) del boton <div?
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        console.log(cursoSeleccionado);
+
+        //desabilitamos el boton
+        e.target.classList.add('disabled')
+        //modificamos el texto del boton
+        e.target.textContent = 'Ir al carrito'
+
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Agregado el curso",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        
+    }
+    
+}
+
+
--- practica_cursos_vanilla/main.js
+++ practica_cursos_vanilla/main.js
@@ -1,4 +1,14 @@
 //importamos bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { agregarCurso } from './carrito';
 
+//utilizamos un evento de escucha para todo el documento HTML
+//domcontentLoaded => esta atento a toda la pagina web (cuando se recargue o se monte)
+document.addEventListener('DOMContentLoaded',() => {
+    
+    //accediendo en el main
+    document.getElementById('lista-cursos').addEventListener('click', agregarCurso)
+
+
+}) 
